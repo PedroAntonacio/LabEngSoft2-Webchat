38076ddbace39eaeb6312bcd77fa26792cd87ea6
PedroAntonacio <p.antonacio@gmail.com>
2021-03-22 18:53:21 -0300

    okteto teste 3

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -9,7 +9,7 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-// const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 443;
 // const ENDPOINT = `localhost:${PORT}`;
 const ENDPOINT = 'https://webchat-okteto-backend-pedroantonacio.cloud.okteto.net:443';
 
